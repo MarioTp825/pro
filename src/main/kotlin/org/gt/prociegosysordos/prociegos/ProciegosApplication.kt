ccec1e3d1b9b7f4fa9e379c10e080ce78e7ee28b
Mario Tepe <marioandresth@yahoo.com>
2021-11-01 21:18:34 -0600

    create: project was created

---
+++ src/main/kotlin/org/gt/prociegosysordos/prociegos/ProciegosApplication.kt
@@ -0,0 +1,11 @@
+package org.gt.prociegosysordos.prociegos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProciegosApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProciegosApplication>(*args)
+}
---
+++ src/test/kotlin/org/gt/prociegosysordos/prociegos/ProciegosApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.gt.prociegosysordos.prociegos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProciegosApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
